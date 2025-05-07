4d2b6fc16aaf47171c520c97777872a44b2e662b
luong <levion287204@gmail.com>
2025-05-07 13:23:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function selectCategory(category) {
+    document.getElementById('screen-start').style.display = 'none';
+    document.getElementById('screen-main').style.display = 'block';
+    console.log("Chọn loại mì:", category);
+  }
